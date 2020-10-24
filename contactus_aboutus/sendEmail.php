a351216b8f12da764a4611cd19300b7c9766cf8b
Piyush Chugeja <piyushchugeja@gmail.com>
2020-10-24 16:20:19 +0530

    PHP mailer bug fixed

--- contactus_aboutus/sendEmail.php
+++ contactus_aboutus/sendEmail.php
@@ -8,34 +8,26 @@ require_once "PHPMailer/Exception.php";
 require_once "PHPMailer/SMTP.php";
 
 if(isset($_POST['submit'])){
-    $name=$_POST['name']; // Get Name value from HTML Form
+    $name=$_POST['name']; 
     $lname=$_POST['lname'];
-    $phone=$_POST['phone'];  // Get Mobile No
-    $email=$_POST['email'];  // Get Email Value
+    $phone=$_POST['phone']; 
+    $email=$_POST['email'];  
     $message=$_POST['message']; // Get Message Value
-      
     $mail = new PHPMailer(true);
 
-    //Enable SMTP debugging.
     $mail->SMTPDebug = 3;    
 
-    //Set PHPMailer to use SMTP.
     $mail->isSMTP();            
-
-    //Set SMTP host name                          
+                         
     $mail->Host = "smtp.gmail.com";
 
-    //Set this to true if SMTP host requires authentication to send email
     $mail->SMTPAuth = true;           
-
-    //Provide username and password     
+  
     $mail->Username = "mailerforthresholdgaming@gmail.com";                 
-    $mail->Password = "webdev@project";  
+    $mail->Password = "mailer123*";  
 
-    //If SMTP requires TLS encryption then set it
     $mail->SMTPSecure = "tls";      
 
-    //Set TCP port to connect to
     $mail->Port = 587;                                   
 
     $mail->From = ($email);
@@ -45,7 +37,7 @@ if(isset($_POST['submit'])){
 
     $mail->isHTML(true);
 
-    $mail->Subject = "A message from $name $lname";
+    $mail->Subject = "A message from: " . $name . " " . $lname;
     $mail->Body = "<i>Sender E-Mail address: $email <br><br> Message: $message <br><br>Phone number: $phone</i>";
 
     try {
